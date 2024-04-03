14ab608c09c2e71f9a84ccde171b4033b9363df6
Karina1220 <kt599076@gmail.com>
2024-04-03 11:17:50 -0600

    catalogos

--- mvvm/viewmodel/FormularioReportes/Pestanas/PestanaReporteViewModel.cs
+++ mvvm/viewmodel/FormularioReportes/Pestanas/PestanaReporteViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Comisión_Estatal_de_Búsqueda_del_Estado_de_Veracruz.core;
+using Comisión_Estatal_de_Búsqueda_del_Estado_de_Veracruz.mvvm.model.FormularioReportes.SenasParticulares;
+using Comisión_Estatal_de_Búsqueda_del_Estado_de_Veracruz.mvvm.model.Ubicaciones;
 using Comisión_Estatal_de_Búsqueda_del_Estado_de_Veracruz.mvvm.viewmodel.FormularioReportes;
 using System;
 using System.Collections.Generic;
@@ -14,6 +16,27 @@ namespace Comisión_Estatal_de_Búsqueda_del_Estado_de_Veracruz.mvvm.viewmodel.F
         public PestanaReporteViewModel(FormularioReportesViewModel formularioReportesViewModel)
         {
             this._formularioReportesViewModel = formularioReportesViewModel;
+            CargarEstado();
+        }
+
+        private Dictionary<string, EstadoData> _estados;
+        public Dictionary<string, EstadoData> Estados
+        {
+            get
+            {
+                return _estados;
+            }
+            set
+            {
+                _estados = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public async void CargarEstado()
+        {
+            var estados = await HttpClientHandler.GetEstados();
+            Estados = (Dictionary<string, EstadoData>)estados;
         }
     }
 }
